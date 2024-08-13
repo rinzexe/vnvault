25450bd4eed40e6b13d90c3fbe9155287c12bae1
rinz <roberts.denisovs@gmail.com>
2024-08-13 19:36:45 +0300

    changing level cal

--- src/utils/levels.ts
+++ src/utils/levels.ts
@@ -2,14 +2,14 @@
 
 export function calculateLevel(xp: number) {
     let level = 1;
-    let xpForNextLevel = 200; // XP required to reach level 2
+    let xpForNextLevel = 500; // XP required to reach level 2
     let remainingXP = xp;
 
     // Loop to determine the current level based on total XP
     while (remainingXP >= xpForNextLevel) {
         remainingXP -= xpForNextLevel;
         level++;
-        xpForNextLevel = Math.floor(xpForNextLevel * 1.2); // Increase XP requirement by 1.5x
+        xpForNextLevel = Math.floor(xpForNextLevel * 1.5); // Increase XP requirement by 1.5x
     }
 
     return {
