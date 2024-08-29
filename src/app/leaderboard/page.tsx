eb5429802f7810a43584f2a5e2e45ee980f0591d
rinz <roberts.denisovs@gmail.com>
2024-08-29 11:56:18 +0300

    leaderboard fix

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -12,6 +12,7 @@ import Row from "../_components/table/table-entry"
 
 export default function Leaderboard() {
     const [leaderboardData, setLeaderboardData] = useState<any>(null)
+    const [isLoading, setIsLoading] = useState<boolean>(true)
 
     const auth = useAuth()
     const router = useRouter()
@@ -20,6 +21,7 @@ export default function Leaderboard() {
         async function fetchLeaderboard() {
             const res = await auth.getLeaderboard()
             setLeaderboardData(res)
+            setIsLoading(false)
         }
 
         fetchLeaderboard()
@@ -27,42 +29,48 @@ export default function Leaderboard() {
 
     return (
         <div className="flex flex-col w-full lg:w-auto items-center gap-8">
-            <h1 className="mb-8">
-                Leaderboard
-            </h1>
-            <Table>
-                <Headers fields={['Longest streak', 'Level']} />
-                {leaderboardData && leaderboardData.map((user: any, id: number) => {
-                    return (
-                        <Row
-                            key={id}
-                            href={"/profile/" + user.username}
-                            avatarUser={user}
-                            fields={[
-                                (
-                                    <h4 key={id} className="hidden text-center lg:block">
-                                        {user.longest_streak}
-                                    </h4>
-                                ),
-                                (
-                                    <div key={id} className="flex flex-row justify-end">
-                                        <div className="">
-                                            <h4>
-                                                {"Level " + calculateLevel(user.xp).level}
-                                            </h4>
-                                            <p className="text-sm text-right text-blue-500">
-                                                {user.xp + " XP"}
-                                            </p>
+            <div className="max-w-[1000px] w-full">
+                <h1 className="mb-8 text-center">
+                    Leaderboard
+                </h1>
+                <Table
+                    isLoading={isLoading}
+                    acceptedTypes={{ row: true, card: false }}
+                    headers={{
+                        fields: ['Longest streak', 'Level']
+                    }}
+                    entries={
+                        leaderboardData && leaderboardData.map((user: any, id: number) => {
+                            return {
+                                key: id,
+                                href: "/profile/" + user.username,
+                                avatarUser: user,
+                                fields: [
+                                    (
+                                        <h4 key={id} className="hidden text-center lg:block">
+                                            {user.longest_streak}
+                                        </h4>
+                                    ),
+                                    (
+                                        <div key={id} className="flex flex-row justify-end">
+                                            <div className="">
+                                                <h4>
+                                                    {"Level " + calculateLevel(user.xp).level}
+                                                </h4>
+                                                <p className="text-sm text-right text-blue-500">
+                                                    {user.xp + " XP"}
+                                                </p>
+                                            </div>
                                         </div>
-                                    </div>
-                                )
-                            ]}
-                            title={user.username}
-                            numbered={true}
-                        />
-                    )
-                })}
-            </Table>
-        </div>
+                                    )
+                                ],
+                                title: user.username,
+                            }
+
+                        })
+                    }
+                />
+            </div>
+        </div >
     )
 }
